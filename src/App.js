4e2835e5cb65ddb5c4ee42b21cddee264eba68c8
Ryan-Nam <alfofh7060@gmail.com>
2021-10-26 23:55:29 +1000

    Add html css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  let posts = '시드니 고기 맛집';
+
   return (
     <div className="App">
       {/* blog: Nav */}
       <div className="black-nav">
         <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> { posts } </h3>
+        <p>10월 26일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { posts } </h3>
+        <p>10월 26일 발행</p>
+        <hr/>
       </div>
     </div>
   );
