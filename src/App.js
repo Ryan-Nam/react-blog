6523ddaf9d29182ee11ad4e8489a89a986d0c8ba
Ryan-Nam <alfofh7060@gmail.com>
2021-10-27 23:50:10 +1000

    Add

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let [따봉, 따봉변경] = useState(0); //0으로 초기화
   let posts = '시드니 고기 맛집';
 
+
   return (
     <div className="App">
       {/* blog: Nav */}
@@ -17,7 +18,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } <span onClick={()=> { 따봉 + 1 }}>👍 </span>0</h3>
+        <h3> { 글제목[0] } <span onClick={()=> { 따봉변경(따봉+1) }}>👍 </span>{따봉}</h3>
         <p>10월 26일 발행</p>
         <hr/>
       </div>
