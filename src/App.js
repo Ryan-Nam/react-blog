7c164a36cad7d9c8cc8a439e62d36457082b2e47
Ryan-Nam <alfofh7060@gmail.com>
2021-10-27 23:47:08 +1000

    Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import React, {useState} from 'react';
 import './App.css';
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['남자코트 추천', '강난 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0); //0으로 초기화
   let posts = '시드니 고기 맛집';
 
   return (
@@ -14,7 +17,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={()=> { 따봉 + 1 }}>👍 </span>0</h3>
         <p>10월 26일 발행</p>
         <hr/>
       </div>
