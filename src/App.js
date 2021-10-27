4115ef5cfc3a856ee8c963561cd602a87966b202
Ryan-Nam <alfofh7060@gmail.com>
2021-10-27 23:35:58 +1000

    Add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 
 function App() {
 
-  useState('남자 코트 추천'); [a,b]
-
-
-
-
-
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강난 우동맛집', '파이썬독학']);
   let posts = '시드니 고기 맛집';
 
   return (
@@ -19,12 +14,17 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { posts } </h3>
+        <h3> { 글제목[0] } </h3>
         <p>10월 26일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { posts } </h3>
+        <h3> { 글제목[1] } </h3>
+        <p>10월 26일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
         <p>10월 26일 발행</p>
         <hr/>
       </div>
